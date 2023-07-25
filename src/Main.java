c11a90ee7cfc39ccff8110f1538acda6695fe908
pooja <poojakewat007@gmail.com>
2023-07-25 19:04:14 +0530

    2 programs

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+       /*
+        *__*
+        *__*
+        *__*
+
+       int n = 3;
+        for (int row = 1; row < n; row++){
+            System.out.print("*");
+
+            for (int space = 1; space <=n-1; space++) {
+                System.out.print("_");
+
+            }
+            System.out.println("*");
+        }*/
+
+        //n-1 space *
+        //n-2 space *
+        //*
+
+      /*
+      __*
+      _**
+      ***
+
+      int n = 3;
+        int counter = 0;
+
+        for (int row = 1; row <= n; row++) {
+            for (int space = 1; space <= n - row; space++) {
+                System.out.print("_");
+            }
+            for (int str = 1; str <= row; str++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+    }
+}
+
+
+
