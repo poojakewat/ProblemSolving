7f927e42130305e40e1965e7bae91f74f368a7db
pooja <poojakewat007@gmail.com>
2023-07-25 19:39:38 +0530

    new pattern problem added

--- src/com/test/core/Main.java
+++ src/com/test/core/Main.java
@@ -1,13 +1,13 @@
+package com.test.core;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
       /*
         *__*
         *__*
         *__*
     */
-       int n = 3;
+     /*  int n = 3;
         for (int row = 1; row < n; row++){
             System.out.print("*");
 
@@ -16,19 +16,20 @@ public class Main {
 
             }
             System.out.println("*");
-        }
+        }*/
 
         //n-1 space *
         //n-2 space *
         //*
 
-      /*
+   /*
       __*
       _**
       ***
 
+    */
+ /*
       int n = 3;
-        int counter = 0;
 
         for (int row = 1; row <= n; row++) {
             for (int space = 1; space <= n - row; space++) {
@@ -40,6 +41,28 @@ public class Main {
             }
             System.out.println();
         }*/
+
+        /*
+            ***
+            _**
+            __*
+
+         */
+
+        int n = 3;
+
+        for (int row = 1; row <= n; row++) {
+
+            for ( int space = 1; space <= row - 1; space++) {
+                System.out.print("_");
+            }
+            for (int str = 1; str<=n-row+1; str++) {
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
     }
 }
 
